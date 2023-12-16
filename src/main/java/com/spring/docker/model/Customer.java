a3f2eac49dca7812467225ed4b1238dc926bf868
Rishabh <rishabhdev444@gmail.com>
2023-12-16 13:17:51 +0530

    First commit

--- src/main/java/com/spring/docker/model/Customer.java
+++ src/main/java/com/spring/docker/model/Customer.java
@@ -1,2 +1,53 @@
-package com.spring.docker.model;public class Customer {
+package com.spring.docker.model;
+
+public class Customer {
+        private Long customerId;
+
+        private String firstName;
+
+        private String lastName;
+
+        private String email;
+
+        public Customer() {
+        }
+
+        public Customer(Long customerId, String firstName, String lastName, String email) {
+                this.customerId = customerId;
+                this.firstName = firstName;
+                this.lastName = lastName;
+                this.email = email;
+        }
+
+        public Long getCustomerId() {
+                return customerId;
+        }
+
+        public void setCustomerId(Long customerId) {
+                this.customerId = customerId;
+        }
+
+        public String getFirstName() {
+                return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+                this.firstName = firstName;
+        }
+
+        public String getLastName() {
+                return lastName;
+        }
+
+        public void setLastName(String lastName) {
+                this.lastName = lastName;
+        }
+
+        public String getEmail() {
+                return email;
+        }
+
+        public void setEmail(String email) {
+                this.email = email;
+        }
 }
